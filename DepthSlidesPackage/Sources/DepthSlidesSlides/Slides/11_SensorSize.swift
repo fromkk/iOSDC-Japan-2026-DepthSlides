79b1bdf632fbd468f462e4f5c2ab08afd36e7619
Kazuya Ueoka <info@fromkk.info>
2026-08-08 16:13:49 +0900

    add sensor sizes

--- DepthSlidesPackage/Sources/DepthSlidesSlides/Slides/11_SensorSize.swift
+++ DepthSlidesPackage/Sources/DepthSlidesSlides/Slides/11_SensorSize.swift
@@ -4,27 +4,43 @@ import SwiftUI
 
 @Slide
 struct SensorSize: View {
+  @Environment(\.slideTheme) var slideTheme
   let converter = MarkdownToSlideConverter()
 
   var body: some View {
-    SlideWrapper {
-      converter.convertPage(
-        """
-        # センサーサイズ
+    HStack {
+      SlideWrapper {
+        converter.convertPage(
+          """
+          # センサーサイズ
 
-        - デジタルカメラには様々なセンサーサイズがある
-          - 1型（1インチ）
-          - マイクロフォーサーズ（4/3型）
-          - APS-C
-          - フルサイズ（35mm判）
-          - 中判（ミディアムフォーマット）
-        - センサーサイズによる違い
-          - 大きい方が高画素で記録可能（センサーをたくさん並べることができる）
-          - 大きい方がたくさんの光を受け取ることができる
-          - 大きい方が被写界深度が浅くなる（ボケやすくなる）
-        """
-      )
+          - デジタルカメラには様々なセンサーサイズがある
+            - 1型（1インチ）
+            - マイクロフォーサーズ（4/3型）
+            - APS-C
+            - フルサイズ（35mm判）
+            - 中判（ミディアムフォーマット）
+          - センサーサイズによる違い
+            - 大きい方が高画素で記録可能（センサーをたくさん並べることができる）
+            - 大きい方がたくさんの光を受け取ることができる
+            - 大きい方が被写界深度が浅くなる（ボケやすくなる）
+          """
+        )
+      }
+
+      VStack(alignment: .leading) {
+        Image(.sensorSizesOverlaidInside)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 600)
+
+        Text("https://en.wikipedia.org/wiki/File:Sensor_sizes_overlaid_inside.svg")
+          .font(slideTheme.bodyFont)
+          .frame(width: 600)
+          .multilineTextAlignment(.leading)
+      }
     }
+    .padding(slideTheme.contentPadding)
   }
 
   var script: String = """
